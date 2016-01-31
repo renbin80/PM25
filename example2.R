6f41601fda15d2d1a331d0719abc3d98411d79ab
renbin80 <renbin@gmail.com>
2016-01-31 19:47:36 +0800

    updated with file source

--- example2.R
+++ example2.R
@@ -1,6 +1,9 @@
+# get files from:https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip
 #plot 1
 #NEI <- readRDS("summarySCC_PM25.rds")
 #SCC <- readRDS("Source_Classification_Code.rds")
+
+
 
 a<-aggregate(Emissions ~ year, NEI, sum )
 print(a)
