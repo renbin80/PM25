a6108bb5e97f99010cf3c9e1e5ff11d1eaa8b325
renbin80 <renbin@gmail.com>
2016-02-01 17:04:27 +0800

    Update example3.R

--- example3.R
+++ example3.R
@@ -6,6 +6,7 @@ scc$SCC <- as.character(scc$SCC)
 scc_coal <- scc[grep("Coal", scc$Short.Name, ignore.case = TRUE), c("SCC","Short.Name")]
 str(scc_coal)
 # Merge two data sets (53,400 records)
+##### Merging data!
 nei_coal <- merge(x = nei, y = scc_coal, by.x = "SCC", by.y = "SCC")
 # Total coal combustion related emissions by year
 nei_coal_year <- aggregate(Emissions ~ year, nei_coal, sum)
